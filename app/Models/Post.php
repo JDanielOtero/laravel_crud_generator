27fd2d1f034b73e6419527df6c1de773cf28b6f8
Daniel Otero <jdanielotero01@gmail.com>
2025-03-29 01:24:14 -0500

    Adding Crud Generator

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,11 +2,31 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Post
+ *
+ * @property $id
+ * @property $title
+ * @property $content
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
 class Post extends Model
 {
-    use HasFactory;
-    protected $fillable = ["title","content"];
+    
+    protected $perPage = 20;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = ['title', 'content'];
+
+
 }
